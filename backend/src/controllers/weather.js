fe366ebf00e7ae914376212e4363546d9af377e2
IvelinaT <ivelina_trifonova@yahoo.com>
2020-03-25 09:28:25 +0200

    get weather fix

--- backend/src/controllers/weather.js
+++ backend/src/controllers/weather.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const config = require('../../config/config.js')
 const { weatherApi: { host, version, apiKey } } = config
 
-weather.getForecast = async () => {
+weather.getForecast = async (params) => {
   try {
     /*
     * You can search 16 day weather forecast with daily average parameters by city name
@@ -11,10 +11,15 @@ weather.getForecast = async () => {
     * but not with a FREE subscription.
     * Please see http://openweathermap.org/faq#error401 for more info.
     */
-    const url = `http://${host}/data/${version}/forecast?q=Sofia&mode=json&units=metric&cnt=7&APPID=${apiKey}`
+    let { city } = params
+    if (!city) {
+      city = 'Sofia'
+    }
+    const url = `http://${host}/data/${version}/forecast?q=${city}&mode=json&units=metric&cnt=7&APPID=${apiKey}`
     const weatherInstance = await axios.get(url)
     return weatherInstance.data
   } catch (error) {
+    statusCode = 500
     console.log(error, 'Failed to get weather')
     throw error
   }
